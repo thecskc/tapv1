26c4999eaf4bcf9cd3f89f03c43b902ca5f18e18
Krish Chelikavada <kchelikavada@gmail.com>
2021-06-15 13:01:41 -0700

    button state

--- components/Connection.js
+++ components/Connection.js
@@ -38,14 +38,47 @@ function Connection(props) {
         });
     }
 
+    const updateDefaultState = function(event){
+        db.collection("connections").doc(props.personTwoUid + props.personOneUid).set({
+            "state": "DEFAULT"
+        }, {
+            merge: true
+        }).then(() => {
+            console.log("Update success!");
+        });
+
+        db.collection("connections").doc(props.personOneUid + props.personTwoUid).set({
+            "state": "DEFAULT"
+        }, {
+            merge: true
+        }).then(() => {
+            console.log("Update success!");
+        });
+    }
+
     if(loading){
         return <div/>
     }
+
+    let statusButton;
+
+    if(props.state === "SEND_REQUEST"){
+
+        statusButton = <button onClick={updateDefaultState}>Join {profile.email} room</button>
+
+    }
+    else if(props.state === "RECEIVE_REQUEST"){
+        statusButton = <button onClick={updateDefaultState}> You have been tapped. Join room</button>
+    }
+    else{
+        statusButton =<button onClick={clickTap}>Tap</button>;
+    }
+
     return (
         <div>
             <div>{profile.email}</div>
-            <div>{props.personTwoData}</div>
-            <button onClick={clickTap}>Tap</button>
+            {statusButton}
+
 
         </div>
     )
